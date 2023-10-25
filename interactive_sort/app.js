277bc4114b6a5f3869ffb05d886a3fcfb033522c
DenMerch <rosignolli@gmail.com>
2023-10-25 15:49:39 +0300

    done

--- interactive_sort/app.js
+++ interactive_sort/app.js
@@ -1,40 +1,81 @@
-const readline = require('node:readline');
-const rl = readline.createInterface({
+const rl = require('readline').createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
 });
 
 let data = null
+let dataText = []
+let dataNumber = []
 
-const sortRequest = " \n1. Sort words alphabetically\n 2. Show numbers from lesser to greater\n 3. Show numbers from bigger to smaller \n 4. Display words in ascending order by number of letters in the word \n 5. Show only unique words"
 
-const interactiveSort = async () => {
+const sortRequest = "\n How would you like to sort values? \n1. Sort words alphabetically\n 2. Show numbers from lesser to greater\n 3. Show numbers from bigger to smaller \n 4. Display words in ascending order by number of letters in the word \n 5. Show only unique words \n Select (1 - 5) end press Enter:"
+const firstQuestion = "Hello. Enter 10 words or digits deviding them in space: "
 
-    console.log('Hello. Enter 10 words or digits deviding them in space:');
-    rl.on('line', (line) => {
-        console.log(`Received: ${line}`);
+function input(text) {
+    return new Promise((callbackFn, errorFn) => {
+        rl.question(text, (uinput) => {
+            callbackFn(uinput);
+        }, () => {
+            errorFn();
+        });
     });
-    // const answer = await rl.question('')
-    // console.log(answer);
-
-    // rl.question('Hello. Enter 10 words or digits deviding them in space: ', async (answer) => {
-
-    //     data = await answer.split(' ')
-    //     rl.pause()
-
-    //     if (data.length < 10) {
-    //         console.log('You entered less then 10 words or digits');
-    //         rl.close();
-    //         return
-    //     }
-    //     console.log("How would you like to sort value?");
-    //     console.log(sortRequest);
-    //     rl.resume()
-
-    //     rl.close();
-    // });
-
 }
 
-interactiveSort()
+const main = async () => {
+    data = await input(firstQuestion);
 
+    if (data.toLowerCase() === 'exit') {
+
+        rl.close();
+        process.exit(0);
+
+    } else {
+        data = data.split(' ')
+
+        data.forEach(element => {
+            if (isNaN(element)) {
+
+                dataText.push(element)
+            }
+            else dataNumber.push(element)
+        })
+    }
+    const number = await input(sortRequest);
+    console.log(number);
+    switch (number) {
+        case '1':
+            const sortedText = dataText.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
+            dataText = []
+            console.log(sortedText);
+            break;
+        case '2':
+            const sortedNumbersIncr = dataNumber.sort((a, b) => a - b)
+            console.log(sortedNumbersIncr);
+            dataNumber = []
+            break;
+        case '3':
+            const sortedNumbersDecr = dataNumber.sort((a, b) => b - a)
+            console.log(sortedNumbersDecr);
+            dataNumber = []
+            break;
+        case '4':
+            const sortedTextByLength = dataText.sort((a, b) => a.length - b.length)
+            dataText = []
+            console.log(sortedTextByLength);
+            break;
+        case '5':
+            const uniqueText = dataText.filter((word, index, arr) => {
+                return index !== arr.indexOf(word)
+            })
+            dataText = []
+            console.log(uniqueText);
+            break;
+
+        default:
+            break;
+    }
+
+    main()
+};
+
+main();
