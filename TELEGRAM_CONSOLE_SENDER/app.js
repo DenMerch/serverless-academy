f5860b1efc49034598232213774bfc7d15251dcf
DenMerch <rosignolli@gmail.com>
2023-10-27 11:29:34 +0300

    Update app.js

--- TELEGRAM_CONSOLE_SENDER/app.js
+++ TELEGRAM_CONSOLE_SENDER/app.js
@@ -2,23 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const { Command } = require("commander");
 
-
 const program = new Command();
-// const { TELEGRAM_BOT_TOKEN } = process.env.TELEGRAM_BOT_TOKEN.trim();
+
 program
     .option("-m --message <text>", "Send text")
     .option("-p --photo <type>", "Send photo")
-
 
 program.parse(process.argv);
 
 const options = program.opts();
 const bot = new TelegramBot("6792456329:AAHEtD-Ser1eOamOzAA_9VePijk2atqDWwM", { polling: false });
 
-
-
 async function botTelegram(options) {
-
     if (options.message !== undefined) {
         console.log(options.message);
         bot.sendMessage(802771051, options.message);
@@ -29,7 +24,6 @@ async function botTelegram(options) {
     else {
         return console.log("Unknown action type!");
     }
-
 }
 
 botTelegram(options)
